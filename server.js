6e94b0e1c1a3d2b77bec5894425cd24f3d132724
Luis Marin <tsuluismarin@gmail.com>
2020-12-11 07:53:02 -0300

    new

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 
-app.use(espress.static(__dirname+'/dist/galleryFront'));
+app.use(express.static(__dirname+'/dist/galleryFront'));
 
 app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + '/dist/galleryFront/index.html'));
